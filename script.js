67c9f7a747db9aff4a1c6f56a4ee9186bfe5a4bf
avanishsinghrajpoot <avanishsinghrajpoot99@gmail.com>
2026-08-12 08:28:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -124,3 +124,64 @@ function toggleTheme() {
     }
 
 }
+/* =========================
+   TYPING ANIMATION
+========================= */
+
+const typingText = document.getElementById("typing-text");
+
+const words = [
+    "B.Tech Student",
+    "Coder",
+    "Web Developer",
+    "Problem Solver"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+
+    const currentWord = words[wordIndex];
+
+    if (!deleting) {
+
+        typingText.textContent =
+            currentWord.substring(0, charIndex + 1);
+
+        charIndex++;
+
+        if (charIndex === currentWord.length) {
+
+            deleting = true;
+
+            setTimeout(typeEffect, 1500);
+
+            return;
+        }
+
+    } else {
+
+        typingText.textContent =
+            currentWord.substring(0, charIndex - 1);
+
+        charIndex--;
+
+        if (charIndex === 0) {
+
+            deleting = false;
+
+            wordIndex++;
+
+            if (wordIndex === words.length) {
+                wordIndex = 0;
+            }
+
+        }
+    }
+
+    setTimeout(typeEffect, deleting ? 60 : 100);
+}
+
+typeEffect();
